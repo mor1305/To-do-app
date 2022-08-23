e6e96b91cf207081961107c2f0aad875fdfaca07
mor1305 <100129055+mor1305@users.noreply.github.com>
2022-08-23 17:10:29 +0300

    update

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const app = express();
 
-const publicPath = path.join(__dirname, "client", "build");
-app.use(express.static(publicPath));
-
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
+
+const publicPath = path.join(__dirname, "client", "build");
+app.use(express.static(publicPath));
 
 const URI = process.env.ATLAS_URI;
 const PORT = process.env.PORT || 5050;
@@ -24,12 +24,12 @@ const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.sendFile();
-// });
-
 app.get("/users", (req, res) => {
   res.send("testing");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
